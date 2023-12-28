bbd6d51f216a859ee8546c3c299d24d6eb108ff6
mahdizadehamir <mehdizadeh.amir11@gmail.com>
2023-12-28 18:08:05 +0330

    SearchBar Added To Project with Logic

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
 import carsData from '../../data/carsData'
 import CarsPage from '../../components/templates/CarsPage'
 import Categories from '../../components/modules/Categories'
+import SearchBar from '../../components/modules/SearchBar'
 export default function index() {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData} />
     </div>
